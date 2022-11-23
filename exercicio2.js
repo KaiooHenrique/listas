cb24c05fee5bcfbf6cb0eb9c29c6f2828ea51850
KaiooHenrique <kaiohenriquepereira09@gmail.com>
2022-11-22 22:18:05 -0300

    first commit

---
+++ exercicio2.js
@@ -0,0 +1,12 @@
+  
+const numeros =  [6,55,7,11,25,65,43,78,52,41,333,854,425];
+
+for (let i = 0; i < numeros.length; i++) {
+    const numero = numeros[i];
+        if(numero % 2 ===0){
+            console.log(numero);
+        
+        }
+
+     
+}
